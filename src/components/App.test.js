19e0f8dfdd1a6edae2162e3fb17cc54a82261e58
Helen Lin <helen.lin@alleninstitute.org>
2024-09-18 17:12:46 -0700

    fix: restore test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -36,7 +36,7 @@ describe('App component', () => {
   it('renders Toolbar and RenderForm child components on default', () => {
     render(<App appVersion={TEST_APP_VERSION} />)
     expect(Toolbar).toHaveBeenCalled()
-    expect(RenderForm).not.toHaveBeenCalled()
+    expect(RenderForm).toHaveBeenCalled()
   })
 
   it('catches and displays error when unable to render form', () => {
